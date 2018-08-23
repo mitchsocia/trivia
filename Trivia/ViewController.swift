95396f9a4e2e97ee97274ce3b52780d7cc884a7d
Mitchell Socia <mitch.socia@detroitlabs.com>
2018-08-23 15:46:59 -0400

    Initial Commit

---
+++ Trivia/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Trivia
+//
+//  Created by Mitchell Socia on 8/23/18.
+//  Copyright © 2018 Mitchell Socia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBOutlet weak var questionLabel: UILabel! 
+        
+    
+    
+}
+
