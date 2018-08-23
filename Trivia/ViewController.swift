6fe3edb9b337d6b9bcbb90f1e46b13c74ece5f84
Mitchell Socia <mitch.socia@detroitlabs.com>
2018-08-23 16:42:42 -0400

    Started array, tied buttons

--- Trivia/ViewController.swift
+++ Trivia/ViewController.swift
@@ -9,9 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var myQuestions = ["Questions 1", "Question 2", "Question 3", "Question 4", "Quetions 5", "Question 6", "Questions 7", "Question 8"]
+   
+    var randomQuestion = [String]()
+    
+
+    @IBOutlet weak var questionLabel: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        questionLabel.text = myQuestions[0]
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,9 +30,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    @IBOutlet weak var questionLabel: UILabel! 
-        
+   
+    @IBAction func option1Button(_ sender: UIButton) {
+    }
     
+    @IBAction func option2Button(_ sender: UIButton) {
+    }
     
+    @IBAction func option3Button(_ sender: UIButton) {
+    }
+    
+    @IBAction func option4Button(_ sender: UIButton) {
+    }
 }
 
